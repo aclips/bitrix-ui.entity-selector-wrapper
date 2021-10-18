7408bc4659e85ad18e24c740d912d9369333e3df
aclips <aclips@ya.ru>
2021-10-18 14:39:15 +0300

    Create config.php

---
+++ js/aclips/ui-selector/config.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+return [
+    "js" => [
+        "./script.js",
+    ],
+    "rel" => [
+        "ui.entity-selector"
+    ]
+];
